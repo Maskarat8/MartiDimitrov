44d0f394c75920e4e39f343ce85750022fc023ce
Maskarat8 <71455730+Maskarat8@users.noreply.github.com>
2020-11-16 13:06:29 +0200

    Add files via upload

---
+++ AccountsTest.java
@@ -0,0 +1,12 @@
+package conko1;
+
+public class AccountsTest {
+
+	public static void main(String[] args) {
+		SavingsAccount marti = new SavingsAccount();
+		marti.deposit(1000);
+		marti.withdraw(200);
+
+	}
+
+}
---
+++ SavingsAccount.java
@@ -0,0 +1,21 @@
+package conko1;
+
+public class SavingsAccount {
+	public double balance;
+	public double interestRate = 0.1;
+	public String name;
+	
+	public void deposit(double sum) {
+		balance += sum;
+		System.out.println("Successful deposit " + sum + " New balance "+ balance);
+	}
+	public void withdraw(double sum) {
+		if (sum < 0 || sum > balance) {
+			System.out.println("Error!");
+			return;
+		}
+		balance -= sum;
+		System.out.println("Successful withdraw " + sum + " New balance "+ balance);
+	}
+
+}
